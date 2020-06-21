2abf7c37248f88d15771555887777b723ba0e975
kaja <shawalpha5@gmail.com>
2020-06-21 21:29:30 +0000

    Updated Travis CI configs

---
+++ test/consumer_SUITE.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Alpha Umaru Shaw <shawalpha5@gmail.com>
+%%% @doc
+%%%
+%%% @end
+%%% Company: Skulup Ltd
+%%% Copyright: (C) 2020
+%%%-------------------------------------------------------------------
+-module(consumer_SUITE).
+-author("Alpha Umaru Shaw").
+
+%% API
+-export([]).
---
+++ test/producer_SUITE.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Alpha Umaru Shaw <shawalpha5@gmail.com>
+%%% @doc
+%%%
+%%% @end
+%%% Company: Skulup Ltd
+%%% Copyright: (C) 2020
+%%%-------------------------------------------------------------------
+-module(producer_SUITE).
+-author("Alpha Umaru Shaw").
+
+%% API
+-export([]).
